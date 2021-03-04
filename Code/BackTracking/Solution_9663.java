7c6c750114704407f2a3f67a9c82386c6217d6f3
KangHoGyun <50542278+KangHoGyun@users.noreply.github.com>
2021-03-04 23:48:16 +0900

    백준 9663번 N-Queen

---
+++ Code/BackTracking/Solution_9663.java
@@ -0,0 +1,22 @@
+package backTracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_9663 {
+	static int N, res;
+	static int []arr;
+	
+	public static boolean isOk(int num) {
+		return false;
+	}
+	public static void findQueen(int num) {
+		
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+	}
+}
