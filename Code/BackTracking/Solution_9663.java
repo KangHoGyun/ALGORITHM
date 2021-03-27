c001eb3f9fc471c67ffb3ca6fb8461fba2dc2a84
KangHoGyun <50542278+KangHoGyun@users.noreply.github.com>
2021-03-28 00:23:42 +0900

    백준 9663번 N-Queen

--- Code/BackTracking/Solution_9663.java
+++ Code/BackTracking/Solution_9663.java
@@ -9,14 +9,31 @@ public class Solution_9663 {
 	static int []arr;
 	
 	public static boolean isOk(int num) {
-		return false;
+		boolean check = true;
+		for(int i = 0 ; i < num; i++) {
+			if(arr[i] == arr[num] || Math.abs(arr[num] - arr[i]) == num - i) {
+				check = false;
+				break;
+			}
+		}
+		return check;
 	}
 	public static void findQueen(int num) {
-		
+		if(num == N) {
+			res++;
+			return;
+		}
+		for(int i=0; i<N; ++i) {
+			arr[num] = i;
+			if(isOk(num))
+				findQueen(num + 1);
+		}
 	}
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		arr = new int[N];
+		findQueen(0);
+		System.out.println(res);
 	}
 }
